3b637dfeee6cdfec6a4c193bb3143115d7e27a06
priyalApplication_demo <priyalkumari78910@gmail.com>
2026-03-20 23:20:30 +0530

    Added math funtions and del operation

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,9 +4,15 @@ let operator = null;
 let resetScreen = false;
 
 const display = document.getElementById('display');
+const opView = document.getElementById('op-view');
 
 function updateDisplay() {
     display.innerText = currentInput;
+    if (operator && previousInput !== '') {
+        opView.innerText = previousInput + " " + operator;
+    } else {
+        opView.innerText = "";
+    }
 }
 
 function appendNumber(num) {
@@ -14,10 +20,22 @@ function appendNumber(num) {
         currentInput = num;
         resetScreen = false;
     } else {
+        if (num === '.' && currentInput.includes('.')) return; // Double dot prevent karein
         currentInput += num;
     }
     updateDisplay();
 }
+
+function deleteLast() {
+    if (resetScreen) return; 
+    if (currentInput.length > 1) {
+        currentInput = currentInput.slice(0, -1);
+    } else {
+        currentInput = '0';
+    }
+    updateDisplay();
+}
+
 
 function clearDisplay() {
     currentInput = '0';
@@ -27,11 +45,24 @@ function clearDisplay() {
 }
 
 function setOperator(op) {
+     if (op === 'x²') {
+        currentInput = (Math.pow(parseFloat(currentInput), 2)).toString();
+        updateDisplay();
+        return;
+    }
+    if (op === '√') {
+        currentInput = (Math.sqrt(parseFloat(currentInput))).toString();
+        updateDisplay();
+        return;
+    }
+
     if (operator !== null) calculate();
     operator = op;
     previousInput = currentInput;
     resetScreen = true;
+    updateDisplay();
 }
+
 
 function calculate() {
     let result;
